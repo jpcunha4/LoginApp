98334efbb28d5a7a2b76078f7ba3f18145054a81
João Paulo Cunha Ávila <jpcunha4@gmail.com>
2019-12-14 23:49:44 -0300

    Login logic structure in MVP

--- LoginTest/Extensions/StringExtension.swift
+++ LoginTest/Extensions/StringExtension.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
-class StringExtension: NSObject {
-
+extension String {
+    var maskedCPF: String {
+        var maskedCPF = self
+        
+        maskedCPF = maskedCPF.replacingOccurrences(of: "(\\d{3})(\\d+)", with: "$1.$2", options: .regularExpression)
+        maskedCPF = maskedCPF.replacingOccurrences(of: "(\\d{3}.\\d{3})(\\d+)", with: "$1.$2", options: .regularExpression)
+        maskedCPF = maskedCPF.replacingOccurrences(of: "(\\d{3}.\\d{3}.\\d{3})(\\d+)", with: "$1-$2", options: .regularExpression)
+        
+        return maskedCPF
+    }
+    
+    var unmaskedCPF: String {
+        return self.replacingOccurrences(of: #"[.-]"#, with: "", options: .regularExpression)
+    }
 }
--- LoginTest/Presenters/LoginPresenter.swift
+++ LoginTest/Presenters/LoginPresenter.swift
@@ -103,21 +103,3 @@ class LoginPresenter: NSObject {
         delegate?.isValidUser(cpf, password)
     }
 }
-
-// MARK: String Extension
-
-extension String {
-    var maskedCPF: String {
-        var maskedCPF = self
-        
-        maskedCPF = maskedCPF.replacingOccurrences(of: "(\\d{3})(\\d+)", with: "$1.$2", options: .regularExpression)
-        maskedCPF = maskedCPF.replacingOccurrences(of: "(\\d{3}.\\d{3})(\\d+)", with: "$1.$2", options: .regularExpression)
-        maskedCPF = maskedCPF.replacingOccurrences(of: "(\\d{3}.\\d{3}.\\d{3})(\\d+)", with: "$1-$2", options: .regularExpression)
-        
-        return maskedCPF
-    }
-    
-    var unmaskedCPF: String {
-        return self.replacingOccurrences(of: #"[.-]"#, with: "", options: .regularExpression)
-    }
-}
